43baaabae11dcac0b504266b24fb54decc3e8683
akomp <akomp@uw.edu>
2024-06-25 20:56:32 -0700

    Used create-react-app and added a hello world page under /hw path

---
+++ react-app/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const path = window.location.pathname;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(path);
+if (path === '/hw') {
+  root.render(
+    <React.StrictMode>
+      <p>Hello, World!</p>
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
